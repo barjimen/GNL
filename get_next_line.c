a4c739289a711f0e3f9b0ae12ae58e3cd0dda3a1
barjimen <barby.jimenez17@gmail.com>
2023-12-13 23:09:26 +0100

    norma arreglao

--- get_next_line.c
+++ get_next_line.c
@@ -6,88 +6,92 @@
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 22:16:30 by barjimen          #+#    #+#             */
-/*   Updated: 2023/12/13 22:54:48 by barjimen         ###   ########.fr       */
+/*   Updated: 2023/12/13 23:07:10 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next.h"
+#include "get_next_line.h"
 
-#define JUMP  "\033[31m\\n\033[0m"
-#define begin "\033[32m[\033[0m"
-#define end "\033[32m] \033[0m"
-void write_buffer(char *str)
+// #define JUMP "\033[31m\\n\033[0m"
+// #define begin "\033[32m[\033[0m"
+// #define end "\033[32m] \033[0m"
+
+// void	write_buffer(char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	write(1, begin, strlen(begin));
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\n')
+// 			write(1, JUMP, strlen(JUMP));
+// 		else
+// 			write(1, &str[i], 1);
+// 		i++;
+// 	}
+// 	write(1, end, strlen(end));
+// }
+
+char	*joiny(char *montoncito, int fd)
 {
-    int i = 0;
-    write(1, begin, strlen(begin));
-    while (str[i])
-    {
-        if (str[i] == '\n')
-             write(1, JUMP, strlen(JUMP));
-        else
-            write(1, &str[i], 1);
-        i++;
-    }
-    write(1, end, strlen(end));
+	char	*buffer;
+	int		aux;
+
+	aux = 1;
+	buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!montoncito)
+		montoncito = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	while (!ft_strchr(buffer, '\n'))
+	{
+		aux = read(fd, buffer, BUFFER_SIZE);
+		if (aux == 0)
+			break ;
+		buffer[aux] = '\0';
+		write_buffer(buffer);
+		montoncito = ft_strjoin(montoncito, buffer);
+	}
+	return (montoncito);
 }
 
-char *joiny(char *montoncito, int fd)
+char	*cutty(char *montoncito)
 {
-    char *buffer;
-    int aux = 1;
+	int		cont;
+	int		i;
+	char	*line;
 
-    buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-    if (!montoncito)
-        montoncito = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-    while (!ft_strchr(buffer, '\n'))
-    {
-        aux = read(fd, buffer, BUFFER_SIZE);
-        if (aux == 0)
-            break ;
-        buffer[aux] = '\0';
-        write_buffer(buffer);
-        montoncito = ft_strjoin(montoncito, buffer);
-    }
-    return(montoncito);
+	cont = 0;
+	i = 0;
+	while (montoncito[cont] != '\0' && montoncito[cont] != '\n')
+		cont++;
+	line = ft_calloc(sizeof(char), cont);
+	while (i < cont)
+	{
+		line[i] = montoncito[i];
+		i++;
+	}
+	return (line);
 }
 
-char *cutty(char *montoncito)
+char	*get_next_line(int fd)
 {
-    int     cont;
-    int     i;
-    char    *line;
+	static char	*montoncito;
+	char		*line;
 
-    cont = 0;
-    i = 0;
-    while (montoncito[cont] != '\0' && montoncito[cont] != '\n')
-        cont++;
-    line = ft_calloc(sizeof(char), cont);
-    while (i < cont)
-    {
-        line[i] = montoncito[i];
-        i++;
-    }
-    return (line);
+	montoncito = joiny(montoncito, fd);
+	line = cutty(montoncito);
+	printf("\nEl valor de line es: %s\n", line);
+	montoncito += ft_strlen(line) + 1;
+	printf("El valor de montoncito es: %s\n", montoncito);
+	return (line);
 }
 
-char *get_next_line (int fd)
+int	main(int argc, char **argv)
 {
-    static char *montoncito;
-    char *line;
-    montoncito = joiny(montoncito, fd); // (0\+ho+la+\nq) -> (q+ue+ta+l\n)
-    line = cutty(montoncito); //((hola\n)q)
-    
-    printf("\nEl valor de line es: %s\n", line);
-    
-    montoncito += ft_strlen(line) + 1;
-    printf("El valor de montoncito es: %s\n", montoncito);
-   return (line); 
-}
+	int	fd;
 
-int main(int argc, char **argv)
-{
-    int fd = open(argv[1], O_RDONLY);
-    get_next_line(fd);
-    get_next_line(fd);
-    
-    return (0);
+	fd = open(argv[1], O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next.h                                         :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 00:13:35 by barjimen          #+#    #+#             */
-/*   Updated: 2023/12/13 22:30:28 by barjimen         ###   ########.fr       */
+/*   Updated: 2023/12/13 23:08:48 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 
 # include "stdio.h"
 # include "unistd.h"
@@ -26,17 +25,12 @@
 #  define BUFFER_SIZE 3
 # endif
 
-
 //OTRAS COSIS
 
-int	    ft_atoi(const char *str);
-
+int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-
 char	*ft_strchr(const char *str, int c);
-
 char	*ft_strjoin(char const *s1, char const *s2);
-
-int	    ft_strlen(char const *str);
+int		ft_strlen(char const *str);
 
 #endif
