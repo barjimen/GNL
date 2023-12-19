f423de5562f2eab0dff61cc27d7c0cedbfa77097
barjimen <barby.jimenez17@gmail.com>
2023-12-19 23:24:08 +0100

    other

---
+++ get_next_line copy.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 22:16:30 by barjimen          #+#    #+#             */
+/*   Updated: 2023/12/19 23:22:47 by barjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	a;
+	char	*b;
+
+	if (!s)
+		return (NULL);
+	a = ft_strlen(s);
+	if (start >= a)
+	{
+		b = ft_calloc(1, 1);
+		if (!b)
+			return (NULL);
+		return (b);
+	}
+	if (len > a)
+		len = a - start;
+	b = ft_calloc(sizeof(char), len + 1);
+	if (!b)
+		return (NULL);
+	b = ft_memcpy(b, &s[start], len);
+	return (b);
+}
+
+char	*joiny(char *montoncito, int fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		aux;
+	char	*temp;
+
+	aux = 0;
+	buffer[aux] = '\0';
+	while (!ft_strchr(buffer, '\n'))
+	{
+		aux = read(fd, buffer, BUFFER_SIZE);
+		if ((aux == -1 || !*buffer))
+		{
+			return (montoncito);
+		}
+		if (aux == 0)
+			break ;
+		buffer[aux] = '\0';
+		if (montoncito)
+		{
+			temp = montoncito;
+			montoncito = ft_strjoin(temp, buffer);
+			free(temp);
+		}
+		else
+		{
+			montoncito = ft_calloc(aux + 1, sizeof(char));
+			ft_memcpy(montoncito, buffer, aux);
+		}
+	}
+	return (montoncito);
+}
+
+char	*cutty(char **montoncito)
+{
+	int		cont;
+	char	*line;
+	char	*tmp;
+
+	cont = 0;
+	while ((*montoncito)[cont] != 0 && (*montoncito)[cont] != '\n')
+		cont++;
+	line = ft_calloc(sizeof(char), cont + 1);
+	if (!line)
+		return (NULL);
+	ft_memcpy(line, *montoncito, cont);
+	if (*montoncito[cont] == '\n')
+		cont++;
+	tmp = ft_substr(*montoncito, cont, -1);
+	free(*montoncito);
+	*montoncito = tmp;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*montoncito;
+	char		*line;
+	char		*tmp;
+
+	line = NULL;
+	if (fd != -1 || BUFFER_SIZE > 0)
+	{
+		montoncito = joiny(montoncito, fd);
+		if (!montoncito)
+			return (NULL);
+		line = cutty(&montoncito);
+	}
+	return (line);
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+	char *str;
+
+	fd = open(argv[1], O_RDONLY);
+	str = get_next_line(fd);
+	while (str)
+	{
+		printf("-%s", str);
+		str = get_next_line(fd);
+		if (!str)
+			break;
+		free(str);
+	}
+
+	return (0);
+}
